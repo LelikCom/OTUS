e6abdd2ff72622e1536ea9140f9dc8aef41ecad8
Ичетовкин Алексей <IchetovkinAlexei@yandex.ru>
2025-02-22 16:51:44 +0500

    app.py fix

--- homework_06/app.py
+++ homework_06/app.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/ping/")
 async def ping():
     return {"message": "pong"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
